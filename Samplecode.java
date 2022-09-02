d00d3edaeac2760253304345e732b56ba5975bd1
Vishwanathms <vishwanath.murthy@gmail.com>
2022-09-02 13:10:17 +0530

    Create Samplecode.java

---
+++ Samplecode.java
@@ -0,0 +1,6 @@
+class Simple{  
+    public static void main(String args[]) throws InterruptedException{  
+     Thread.sleep(5000);
+     System.out.println("Hello Java updated on the master branch - Nov 16th 2021");  
+    }  
+}
